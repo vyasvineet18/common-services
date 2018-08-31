31021e69a4348252c0535a946ebe9d125fe716d1
Vinit <vinit.vyas@softwebsolutions.com>
2018-09-01 02:22:31 +0530

    Initial commmit for common user service

---
+++ user-servc/src/main/java/com/service/user/userservc/UserServcApplication.java
@@ -0,0 +1,12 @@
+package com.service.user.userservc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServcApplication.class, args);
+	}
+}
